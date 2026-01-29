35d9002376cb866e7e6472db3df2407f93d715d8
Danfzj71 <danyflowsimancas@gmail.com>
2026-01-29 01:17:47 -0400

    feat: servidor básico funcionando

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs'); // Para renderizar HTML dinámico
+app.use(express.static('public')); // Para tu CSS e imágenes
+
+app.get('/', (req, res) => {
+    res.send('E-commerce Home Page');
+});
+
+app.listen(3000, () => console.log('Servidor en http://localhost:3000'));
