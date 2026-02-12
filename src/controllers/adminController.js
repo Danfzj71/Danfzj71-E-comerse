07aad8927059e6e1e055d858e6c9b303cc5a31e6
Danfzj71 <danyflowsimancas@gmail.com>
2026-02-11 22:06:44 -0400

    panel de administracion agregado

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -1,14 +1,27 @@
+// src/Controllers/adminController.js
 const Product = require('../models/productModel');
 
 const adminController = {
     showAddProduct: (req, res) => {
-        res.render('admin/addProduct'); 
+        res.render('admin/add-product', { error: null });
     },
 
     createProduct: (req, res) => {
         const { name, description, price, image_url } = req.body;
+
+        // Validar precio en el servidor
+        if (parseFloat(price) <= 0) {
+            return res.render('admin/add-product', { 
+                error: "El precio debe ser un número mayor a cero." 
+            });
+        }
+
         Product.create({ name, description, price, image_url }, (err) => {
-            if (err) return res.status(500).send("Error al guardar");
+            if (err) {
+                return res.render('admin/add-product', { 
+                    error: "Error al guardar en la base de datos." 
+                });
+            }
             res.redirect('/');
         });
     }
--- src/models/productModel.js
+++ src/models/productModel.js
@@ -1,12 +1,12 @@
 const db = require('./sqlite');
 
 const Product = {
+    // Método para obtener todos los productos
     getAll: (callback) => {
         db.all("SELECT * FROM products", [], callback);
     },
-    getById: (id, callback) => {
-        db.get("SELECT * FROM products WHERE id = ?", [id], callback);
-    },
+
+    // El método create que ya teníamos
     create: (data, callback) => {
         const { name, description, price, image_url } = data;
         db.run(
@@ -17,4 +17,5 @@ const Product = {
     }
 };
 
+// ¡MUY IMPORTANTE! Sin esto, el controlador recibirá un objeto vacío
 module.exports = Product;
