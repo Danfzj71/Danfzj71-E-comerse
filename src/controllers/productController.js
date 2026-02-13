c32704669e27894eca83c06e3ba1d28094880ca7
Danfzj71 <danyflowsimancas@gmail.com>
2026-02-12 23:34:06 -0400

    version funcional 1.1

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,4 +1,30 @@
 const Product = require('../models/productModel');
+const db = require('../models/sqlite');
+
+const productController = {
+    showHome: (req, res) => {
+        Product.getAll((err, rows) => {
+            if (err) return res.status(500).send("Error en la base de datos");
+            res.render('index', { listaProductos: rows });
+        });
+    },
+    showDetail: (req, res) => {
+        const id = req.params.id;
+        db.get("SELECT * FROM products WHERE id = ?", [id], (err, producto) => {
+            if (err || !producto) {
+                return res.redirect('/');
+            }
+            res.render('product-detail', { producto: producto });
+            //res.render('product-detail', { producto });
+        });
+    }
+
+};
+
+module.exports = productController;
+
+/**
+ *const Product = require('../models/productModel');
 
 const productController = {
     showHome: (req, res) => {
@@ -16,3 +42,5 @@ const productController = {
 };
 
 module.exports = productController;
+ 
+ */
